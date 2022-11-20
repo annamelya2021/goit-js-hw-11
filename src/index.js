d53889f16e7ca482e57d718312f886aee8ffb1a9
annamelya2021 <anna.basyuk@gmail.com>
2022-11-20 16:36:16 +0200

    html

--- src/index.js
+++ src/index.js
@@ -1,38 +1,17 @@
-// import { fetchCountries } from './fetchCountries';
-// import { refs } from './refs';
-// import { renderCountryList, renderCountryInfo } from './markup';
-
-// import debounce from 'lodash.debounce'; //
-// console.log(debounce);
-// import { Notify } from 'notiflix';
-// console.log(Notify);
-
-// const DEBOUNCE_DELAY = 300;
-
-// refs.input.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY));
-
-// function onCountryInput(e) {
-//   const name = e.target.value.trim();
-//   if (name === '') {
-//     return (refs.ul.innerHTML = ''), (refs.div.innerHTML = '');
-//   }
-
-//   fetchCountries(name)
-//     .then(countries => {
-//       refs.ul.innerHTML = '';
-//       refs.div.innerHTML = '';
-//       if (countries.length === 1) {
-//         refs.ul.insertAdjacentHTML('beforeend', renderCountryList(countries));
-//         refs.div.insertAdjacentHTML('beforeend', renderCountryInfo(countries));
-//       } else if (countries.length >= 10) {
-//         Notify.info(
-//           'Too many matches found. Please enter a more specific name.'
-//         );
-//       } else {
-//         refs.ul.insertAdjacentHTML('beforeend', renderCountryList(countries));
-//       }
-//     })
-//     .catch(error => {
-//       Notify.failure('Oops, there is no country with that name');
-//     });
-// }
+//  <div class="photo-card">
+//   <img src="" alt="" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div>
